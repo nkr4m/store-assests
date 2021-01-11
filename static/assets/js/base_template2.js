ff5a82a1be3e0a81aafce79566e4a34259ad2d60
N.K Ramachandran <nnkrama11@gmail.com>
2021-01-11 15:01:09 +0530

    files

---
+++ static/assets/js/base_template2.js
@@ -0,0 +1,37 @@
+const body = document.querySelector("body");
+const navbar = document.querySelector(".navbar");
+const menuBtn = document.querySelector(".menu-btn");
+const cancelBtn = document.querySelector(".cancel-btn");
+menuBtn.onclick = ()=>{
+  navbar.classList.add("show");
+  menuBtn.classList.add("hide");
+  body.classList.add("disabled");
+}
+cancelBtn.onclick = ()=>{
+  body.classList.remove("disabled");
+  navbar.classList.remove("show");
+  menuBtn.classList.remove("hide");
+}
+window.onscroll = ()=>{
+  this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
+}
+
+
+const next=document.querySelector('#next')
+const prev=document.querySelector('#prev')
+
+function handleScrollNext (direction) {
+const cards = document.querySelector('.card-content')
+cards.scrollLeft=cards.scrollLeft += window.innerWidth / 2 > 600 ? window.innerWidth /2 : window.innerWidth -100
+}
+
+function handleScrollPrev (direction) {
+const cards = document.querySelector('.card-content')
+cards.scrollLeft=cards.scrollLeft -= window.innerWidth / 2 > 600 ? window.innerWidth /2 : window.innerWidth -100
+}
+
+next.addEventListener('click', handleScrollNext)
+prev.addEventListener('click', handleScrollPrev)
+
+
+
